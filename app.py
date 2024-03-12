bb471ba9523bbc3043042f34904a2fe06bbf07ff
keivozazitis <117094580+keivozazitis@users.noreply.github.com>
2024-03-12 12:55:57 +0000

    flask updateots lai register kaut cik strada

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if (request.method == 'POST'):
+        print(request.form['fullname'])
+    # jaatrod kodu, ka sanemt datus no html formas uz python puses (python kodaa)
+
+    return render_template("register.html", title="Registration")
