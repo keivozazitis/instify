9f2996e01f41dcc9ed5e19c318ee4b291b296872
keivozazitis <117094580+keivozazitis@users.noreply.github.com>
2024-05-08 15:03:20 +0000

    principa pabeigts projekts, atliek frontendu biki

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import requests # type: ignore
+import json
+from random import randint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_1.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "IUFBAINnfnauionfuanUINFI"
 API_KEY = "DeC83FkuGywLwbTUX3nazG6bQh9OjVjt1dLAtZ5ech9h31dn1gYgnned"
-API_URL = 'https://api.pexels.com/v1/curated?per_page=5'
+
 
 
 
@@ -36,7 +38,7 @@ def login():
         user = users.query.filter_by(username = username).first()
 
         if user is None:
-            flash("invalid email")
+            flash("invalid username")
             return redirect(request.referrer)
         elif user.password != password:
             flash('invalid password')
@@ -86,7 +88,20 @@ def logout():
 def view():
     return render_template("database.html", values=users.query.all())
 
+def loadPhotos(ammount):
+    API_URL = f'https://api.pexels.com/v1/curated?per_page={ammount}'
+    headers = {
+        'Authorization': API_KEY
+        }
+    res = requests.get(API_URL, headers=headers)
+    if res.status_code != 200:
+        print(f'Error: {res.status_code}')
+        
+    data = res.json()
+    return data
 
+
+sort_by = 'comments'
 @app.route("/instify")
 def home():
 
@@ -95,26 +110,42 @@ def home():
     
     username = session['user_username']
 
-    headers = {
-    'Authorization': API_KEY
-    }
-    res = requests.get(API_URL, headers=headers)
-    if res.status_code != 200:
-        print(f'Error: {res.status_code}')
-    
-    data = res.json()
-    photo_urls = []
+    data = loadPhotos(200)
+    post_data = []
     for photo in data['photos']:
-        photo_urls.append(photo['src']['original'])
+        likes = randint(1, 1000)
+        comments = randint(1, 100)
+        shares = randint(1, 50)
+        x = f'{{"url": "{photo["src"]["original"]}", "name": "{photo["photographer"]}", "likes": "{likes}", "comments": "{comments}", "shares": "{shares}"}}'
+        parsed = json.loads(x)
+        post_data.append(parsed)
     
-    print(photo_urls)
+    global sort_by
+    post_data = sorted(post_data, key=lambda x: int(x[sort_by]), reverse=True)
 
 
+    return render_template("instify.html", username=username, posts=post_data)
 
+@app.route('/sort_by/<type>')
+def sortBy(type):
+    global sort_by
+    sort_by = type
+    return redirect(url_for('home'))
 
-    return render_template("instify.html", username=username, photos=photo_urls)
-
-
+@app.route('/deleteProfile', methods=['GET','POST'])
+def deleteProfile():
+    print(session['user_id'])
+    if request.method == 'GET':
+        id = session.get('user_id')
+        if id is not None:
+            user = users.query.filter_by(_id=id).first()
+            if user is not None:
+                db.session.delete(user)
+                db.session.commit()
+                session.pop('user_id')
+                session.pop('user_username')
+                return redirect(url_for('login'))
+    return "Error: Profile deletion failed."
 
 if __name__ == "__main__":
     with app.app_context():
